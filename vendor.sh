fdb2f16a184d12dd1577ccdfe61a9cf275119fd2
Christian Green <christian@fanhero.com>
2016-09-22 14:26:02 -0400

    Updated build instructions

--- vendor.sh
+++ vendor.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t fanhero/opencvvendor:latest .
-docker run --rm -v data:/vendoring fanhero/opencvvendor:latest
+#docker run --rm -v data:/vendoring fanhero/opencvvendor:latest
+
